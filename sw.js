7517a9114aeb786bf9b076c7f1d9aa803ad32f36
loading <dsrebnik@gmail.com>
2020-12-04 16:08:31 +0000

    service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const filesToCache =
+const PRECACHE_URLS =
 `manifest.json
 ./test.js
 ./index.js
@@ -169,21 +169,47 @@ const filesToCache =
 ./rhythmhack/lang/de/locale.swf`
 .replace(/(\r\n|\n|\r)/gm, "").split("./")
 
+const PRECACHE = 'precache-v2';
+const RUNTIME = 'runtime';
 
 self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
-    caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', function(event) {
+
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-        caches.match(event.request).then(function(response){
-            return response || fetch(event.request);
-        })
-    )
-})
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
