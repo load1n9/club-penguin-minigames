fd9880c35c87b5379d358da1591b187aa4800630
loading <dsrebnik@gmail.com>
2021-01-08 16:39:41 +0000

    fixed

--- sw.js
+++ sw.js
@@ -4,26 +4,12 @@ const PRECACHE_URLS =
 ./index.js
 ./data.js
 ./index.html
-./icefishing/title.swf
-./icefishing/bootstrap.swf
-./icefishing/index.html
-./icefishing/canoworms.swf
-./icefishing/IceFishing.swf
-./icefishing/lang/en/locale.swf
 ./src/7a7b47acdad52ed286ce.module.wasm
 ./src/core.ruffle.75408f40d9423287edd6.js
 ./src/ruffle.js.map
 ./src/ruffle.js
 ./src/core.ruffle.75408f40d9423287edd6.js.map
-./dancecontest/index.html
-./dancecontest/dance.swf
-./dancecontest/lang/en/locale.swf
-./dancecontest/songs/songData2.swf
-./dancecontest/songs/songData4.swf
-./dancecontest/songs/songData1.swf
-./dancecontest/songs/songData6.swf
-./dancecontest/songs/songData5.swf
-./dancecontest/songs/songData3.swf`
+`
 .replace(/(\r\n|\n|\r)/gm, "").split("./")
 
 const PRECACHE = 'precache-v2';
